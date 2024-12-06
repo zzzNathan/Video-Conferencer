dca982c0f70f9c5c59bcebaac0753fa06b6f2b6b
Jonathan <jotkasongo@gmail.com>
2024-12-06 20:52:50 +0000

    progress

--- Video-Conferencer/src/main.jsx
+++ Video-Conferencer/src/main.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./components/Home.jsx"
 import Registration from "./components/Registration.jsx"
 
+// Path is an extension that goes after our URL,
+// once this extension is written the corresponding
+// React element will be loaded and rendered.
 const router = createBrowserRouter([
   {
     path: "/",
